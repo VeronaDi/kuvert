d5b104e7dc3255a095de25b3400a58467c2a4f0f
Diana <diana.didijtsuk@gmail.c0m>
2020-04-29 16:17:06 +0300

    map section added block with contact info

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -9,17 +9,52 @@ import Footer from "../components/Footer"
 import BtnSend from "../components/BtnSend"
 
 import plane from "../images/plane.png"
+import call from "../images/call.png"
+import location from "../images/location.png"
+import write from "../images/write_us.png"
 
 import { css } from "@emotion/core"
 
 import { useTranslation } from "react-i18next"
 
 const cities = [
-  { name: "Ivano-Frankivsk", position: [48.9064118, 24.7253379] },
-  { name: "Kyiv", position: [50.4214245, 30.3981391] },
-  { name: "Kharkiv", position: [49.977438, 36.280344] },
-  { name: "Dnipro", position: [48.458113, 35.070495] },
-  { name: "Lviv", position: [49.806572, 24.064176] },
+  {
+    name: "Ivano-Frankivsk",
+    position: [48.9064118, 24.7253379],
+    phoneOffice: "+38 050 377 08 01",
+    phone: "+38 067 327 08 01",
+    location: "Ivano-Frankivsk Makukha Str, 6",
+    email: "info@kuvert.if.ua",
+  },
+  {
+    name: "Kyiv",
+    position: [50.4214245, 30.3981391],
+    phoneOffice: "+38 067 009 08 01",
+    location: 'м. Київ, вул. Сім"ї Сосніних, 9',
+    email: "kuvert.kiev@gmail.com",
+  },
+  {
+    name: "Kharkiv",
+    position: [49.977438, 36.280344],
+    phoneOffice: "+38 050 433 25 66",
+    location: "м. Харків, вул. Плеханівська, 117",
+    email: "kuvert.kharkiv@gmail.com",
+  },
+  {
+    name: "Dnipro",
+    position: [48.458113, 35.070495],
+    phoneOffice: "+38 050 339 20 21",
+    location: "м. Дніпро, вул. Акінфієва, 18",
+    email: "kuvert.dn@gmail.com",
+  },
+  {
+    name: "Lviv",
+    position: [49.806572, 24.064176],
+    phoneOffice: "+38 0322 70 91 91",
+    phone: "+38 050 317 03 87",
+    location: "м. Львів, вул. Зелена 251",
+    email: "konwert@ukr.net",
+  },
 ]
 
 export default props => {
@@ -46,8 +81,8 @@ export default props => {
         <div
           css={css`
             width: 783px;
-            height: 68px;
             margin: 0 auto;
+            padding: 35px 0;
           `}
         >
           {cities.map((city, index) => (
@@ -80,27 +115,145 @@ export default props => {
         </div>
         <div
           css={css`
-            width: 100%;
-            height: 689px;
+            position: relative;
           `}
         >
-          {typeof window !== "undefined" && (
-            <Map center={city.position} zoom={15}>
-              <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              />
-              <Marker position={city.position}>
-                <Popup
+          <div
+            css={css`
+              width: 100%;
+              height: 689px;
+              position: relative;
+              z-index: 1;
+            `}
+          >
+            {typeof window !== "undefined" && (
+              <Map center={city.position} zoom={13}>
+                <TileLayer
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={city.position}>
+                  <Popup
+                    css={css`
+                      color: red;
+                    `}
+                  >
+                    Kuvert-Ukraine
+                  </Popup>
+                </Marker>
+              </Map>
+            )}
+          </div>
+          <div
+            css={css`
+              width: 432px;
+              height: 397px;
+              position: absolute;
+              left: 91px;
+              top: 84px;
+              background: rgba(56, 56, 56, 0.732);
+              z-index: 5;
+            `}
+          >
+            <div
+              css={css`
+                width: 100%;
+                height: 132px;
+                padding: 30px 0 30px 130px;
+                background: url(${call}) 40px center no-repeat;
+                color: white;
+                font-weight: bold;
+              `}
+            >
+              {t("call")}
+              <div
+                css={css`
+                  padding-top: 24px;
+                `}
+              >
+                {cities.map((city, index) => (
+                  <a
+                    onClick={() => setCityIndex(index)}
+                    href="tel:{city.phoneOffice}"
+                    css={css`
+                      text-decoration: none;
+                      color: white;
+                      font-size: 14px;
+                      padding-right: 5px;
+                      font-weight: normal;
+                    `}
+                  >
+                    {city.phoneOffice}
+                  </a>
+                ))}
+                {cities.map((city, index) => (
+                  <a
+                    onClick={() => setCityIndex(index)}
+                    href="tel:{city.phone}"
+                    css={css`
+                      text-decoration: none;
+                      color: white;
+                      font-size: 14px;
+                      font-weight: normal;
+                    `}
+                  >
+                    {city.phone}
+                  </a>
+                ))}
+              </div>
+            </div>
+            <div
+              css={css`
+                width: 100%;
+                height: 132px;
+                padding: 30px 0 30px 130px;
+                background: url(${location}) 40px center no-repeat;
+                color: white;
+                font-weight: bold;
+              `}
+            >
+              {t("visitUs")}
+              {cities.map((city, index) => (
+                <p
+                  onClick={() => setCityIndex(index)}
                   css={css`
-                    color: red;
+                    font-size: 14px;
+                    font-weight: normal;
                   `}
                 >
-                  Kuvert-Ukraine
-                </Popup>
-              </Marker>
-            </Map>
-          )}
+                  {city.location}
+                </p>
+              ))}
+            </div>
+            <div
+              css={css`
+                width: 100%;
+                height: 132px;
+                padding: 30px 0 30px 130px;
+                background: url(${write}) 40px center no-repeat;
+                color: white;
+                font-weight: bold;
+              `}
+            >
+              {t("writeUs")}
+              {cities.map((city, index) => (
+                <a
+                  onClick={() => setCityIndex(index)}
+                  href="mailto:{city.email}"
+                  css={css`
+                    text-decoration: none;
+                    color: white;
+                    font-size: 14px;
+                    display: block;
+                    padding-top: 18px;
+                    font-weight: normal;
+                  `}
+                >
+                  {city.email}
+                </a>
+              ))}
+            </div>
+          </div>
         </div>
       </section>
       <section
