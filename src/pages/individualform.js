da0d95e18708dd0b80c8f202639ea56a93d5a2a5
Diana <diana.didijtsuk@gmail.c0m>
2020-05-05 16:14:48 +0300

    added english versions of pages

--- src/pages/individualform.js
+++ src/pages/individualform.js
@@ -3,6 +3,8 @@ import { Link } from "gatsby"
 // import { useStaticQuery, graphql } from "gatsby"
 // import Img from "gatsby-image"
 
+import MainNav from "../components/MainNav"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -10,7 +12,13 @@ import { css } from "@emotion/core"
 
 import { useTranslation } from "react-i18next"
 
+import InputAmount from "../components/InputAmount"
+import InputPrintColors from "../components/InputPrintColors"
+import InputDragDrop from "../components/InputDragDrop"
+import BtnNext from "../components/BtnNext"
+
 import logoGrey from "../images/logoGrey.png"
+import arrowSelect from "../images/arrow-select.png"
 import ContactForm from "../components/ContactForm"
 import BtnSendForm from "../components/BtnSendForm"
 
@@ -38,6 +46,7 @@ const IndexPage = props => {
   return (
     <Layout>
       <SEO title="Request" />
+      <MainNav isHome {...props} />
       <Link
         css={css`
           background: url(${logoGrey}) center center no-repeat;
@@ -96,7 +105,7 @@ const IndexPage = props => {
             `}
           >
             2
-          </span>{" "}
+          </span>
           {t("requestOrderCreate")}
         </div>
         <div
@@ -125,11 +134,254 @@ const IndexPage = props => {
           text-align: center;
         `}
       >
-        {t("individualOrder")}
+        {t("printedOrder")}
       </h1>
-      <section></section>
-      <ContactForm />
-      <BtnSendForm />
+      <form
+        action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSejJ5AVb5L0SeEnQzCxJO-HR_VXDXj5PTQmF09fDYN7qz8jwQ/formResponse"
+        method="POST"
+        css={css`
+          position: absolute;
+          left: 50%;
+          -webkit-transform: translate(-50%, -2%);
+          transform: translate(-50%, -2%);
+        `}
+      >
+        <h3
+          css={css`
+            text-align: center;
+            margin-bottom: 40px;
+          `}
+        >
+          {t("envelopeDetails")}
+        </h3>
+        <div
+          css={css`
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            width: 100%;
+            max-width: 780px;
+          `}
+        >
+          <div
+            css={css`
+              display: flex;
+              flex-direction: column;
+              padding-right: 19px;
+            `}
+          >
+            <label
+              css={css`
+                font-size: 12px;
+                padding-bottom: 5px;
+              `}
+            >
+              {t("size")}
+            </label>
+            <select
+              name="entry.1177867946"
+              css={css`
+                -webkit-appearance: none;
+                -moz-appearance: none;
+                appearance: none;
+                background: url(${arrowSelect}), #ffffff;
+                background-position: 97%;
+                background-repeat: no-repeat;
+                width: 380px;
+                height: 65px;
+                font-size: 16px;
+                border: 1px solid #d6d6d6;
+                padding: 18px;
+                outline: none;
+                color: #444444;
+                :focus {
+                  border: 2px solid #c4c4c4;
+                }
+              `}
+            >
+              <option disabled selected>
+                {t("chooseSize")}
+              </option>
+              <option value="C6">C6 (114x162 {t("mm")})</option>
+              <option value="DL">DL (110x220 {t("mm")})</option>
+              <option value="C65">C65 (114x229 {t("mm")})</option>
+              <option value="C5">C5 (162x229 {t("mm")})</option>
+              <option value="C4">C4 (229x324 {t("mm")})</option>
+              <option value="other">{t("other")}</option>
+            </select>
+          </div>
+          <InputAmount />
+        </div>
+        <div
+          css={css`
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            width: 100%;
+            max-width: 780px;
+            padding-top: 40px;
+          `}
+        >
+          <div
+            css={css`
+              display: flex;
+              flex-direction: column;
+              padding-right: 19px;
+            `}
+          >
+            <label
+              css={css`
+                font-size: 12px;
+                padding-bottom: 5px;
+              `}
+            >
+              {t("sealing")}
+            </label>
+            <select
+              name="entry.1177867946"
+              css={css`
+                -webkit-appearance: none;
+                -moz-appearance: none;
+                appearance: none;
+                background: url(${arrowSelect}), #ffffff;
+                background-position: 97%;
+                background-repeat: no-repeat;
+                width: 380px;
+                height: 65px;
+                font-size: 16px;
+                border: 1px solid #d6d6d6;
+                padding: 18px;
+                outline: none;
+                color: #444444;
+                :focus {
+                  border: 2px solid #c4c4c4;
+                }
+              `}
+            >
+              <option disabled selected>
+                {t("chooseGlue")}
+              </option>
+              <option value="mk">{t("mk")}</option>
+              <option value="sk">{t("sk")}</option>
+              <option value="skl">{t("skl")}</option>
+              <option value="bk">{t("bk")}</option>
+            </select>
+          </div>
+        </div>
+
+        <h3
+          css={css`
+            text-align: center;
+            margin: 40px 0;
+          `}
+        >
+          {t("printDetails")}
+        </h3>
+        <div
+          css={css`
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            width: 100%;
+            max-width: 780px;
+          `}
+        >
+          <div
+            css={css`
+              display: flex;
+              flex-direction: column;
+              padding-right: 19px;
+            `}
+          >
+            <label
+              css={css`
+                font-size: 12px;
+                padding-bottom: 5px;
+              `}
+            >
+              {t("colorsFront")}
+            </label>
+            <InputPrintColors />
+          </div>
+          <div
+            css={css`
+              display: flex;
+              flex-direction: column;
+            `}
+          >
+            <label
+              css={css`
+                font-size: 12px;
+                padding-bottom: 5px;
+              `}
+            >
+              {t("colorsBack")}
+            </label>
+            <InputPrintColors />
+          </div>
+        </div>
+        <InputDragDrop />
+
+        <h3
+          css={css`
+            text-align: center;
+            margin: 40px 0;
+          `}
+        >
+          {t("additionalDetails")}
+        </h3>
+        <div
+          css={css`
+            width: 100%;
+            max-width: 780px;
+            display: flex;
+            flex-direction: column;
+          `}
+        >
+          <label
+            css={css`
+              font-size: 12px;
+              padding-bottom: 5px;
+            `}
+          >
+            {t("additionalInfo")}
+          </label>
+          <textarea
+            placeholder="Additional comments, questions, important information, etc."
+            css={css`
+              width: 100%;
+              height: 191px;
+              background: #ffffff;
+              border: 1px solid #d6d6d6;
+              color: #444444;
+              padding: 20px;
+              outline: none;
+              resize: none;
+              ::-webkit-input-placeholder {
+                font-size: 16px;
+                color: #9d9d9d;
+              }
+              :focus {
+                border: 2px solid #c4c4c4;
+              }
+            `}
+          ></textarea>
+        </div>
+        <div
+          css={css`
+            width: 100%;
+            max-width: 780px;
+            margin: 40px 0;
+            display: block;
+            text-align: center;
+          `}
+        >
+          <BtnNext />
+        </div>
+        <button type="submit">submit</button>
+        <ContactForm />
+        <BtnSendForm />
+      </form>
     </Layout>
   )
 }
