a53ee0c3f760858be9b2dc834bfd442eead7d147
Diana <diana.didijtsuk@gmail.c0m>
2020-05-28 16:02:38 +0300

    added code to ecobag white

--- src/data/ecobagWhite.js
+++ src/data/ecobagWhite.js
@@ -1,6 +1,6 @@
 export default [
   {
-    code: "01",
+    code: "T16300020",
     size: "160x80x390",
     color: { en: "white", uk: "білий" },
     handle: { en: "twisted", uk: "кручена" },
@@ -9,16 +9,16 @@ export default [
     price: { en: "from 4 429 UAH", uk: "від 4 429 грн" },
   },
   {
-    code: "02",
+    code: "T11000020",
     size: "180x80x225",
     color: { en: "white", uk: "білий" },
     handle: { en: "twisted", uk: "кручена" },
-    gsm: "90",
+    gsm: "100",
     pack: "400",
     price: { en: "from 3 777 UAH", uk: "від 3 777 грн" },
   },
   {
-    code: "03",
+    code: "220110280_1",
     size: "220x110x280",
     color: { en: "white", uk: "білий" },
     handle: { en: "flat", uk: "плоска" },
@@ -27,7 +27,7 @@ export default [
     price: { en: "from 2 656 UAH", uk: "від 2 656 грн" },
   },
   {
-    code: "04",
+    code: "10411040000",
     size: "240x100x320",
     color: { en: "white", uk: "білий" },
     handle: { en: "twisted", uk: "кручена" },
@@ -36,7 +36,7 @@ export default [
     price: { en: "from 2 656 UAH", uk: "від 2 656 грн" },
   },
   {
-    code: "05",
+    code: "E2001100",
     size: "240x100x360",
     color: { en: "white", uk: "білий" },
     handle: { en: "twisted", uk: "кручена" },
@@ -45,7 +45,7 @@ export default [
     price: { en: "from 4 892 UAH", uk: "від 4 892 грн" },
   },
   {
-    code: "06",
+    code: "E3001100",
     size: "305x170x340",
     color: { en: "white", uk: "білий" },
     handle: { en: "twisted", uk: "кручена" },
@@ -54,7 +54,7 @@ export default [
     price: { en: "from 6 454 UAH", uk: "від 6 454 грн" },
   },
   {
-    code: "07",
+    code: "E4001100",
     size: "305x170x425",
     color: { en: "white", uk: "білий" },
     handle: { en: "twisted", uk: "кручена" },
@@ -63,7 +63,7 @@ export default [
     price: { en: "from 7 360 UAH", uk: "від 7 360 грн" },
   },
   {
-    code: "08",
+    code: "E7001100",
     size: "400x180x390",
     color: { en: "white", uk: "білий" },
     handle: { en: "twisted", uk: "кручена" },
@@ -72,21 +72,12 @@ export default [
     price: { en: "from 8 449 UAH", uk: "від 8 449 грн" },
   },
   {
-    code: "09",
+    code: "E8001100",
     size: "500x180x390",
     color: { en: "white", uk: "білий" },
     handle: { en: "twisted", uk: "кручена" },
     gsm: "100",
     pack: "100",
     price: { en: "from 9 843 UAH", uk: "від 9 843 грн" },
-  },
-  {
-    code: "10",
-    size: "160x80x390",
-    color: { en: "white", uk: "білий" },
-    handle: { en: "twisted", uk: "кручена" },
-    gsm: "90",
-    pack: "250",
-    price: { en: "from 3 799 UAH", uk: "від 3 799 грн" },
   },
 ]
