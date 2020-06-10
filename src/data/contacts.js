2ef9b4b860b5a1bf8f11459d218697764424a870
Diana <diana.didijtsuk@gmail.c0m>
2020-06-10 18:21:08 +0300

    added data contacts

---
+++ src/data/contacts.js
@@ -0,0 +1,40 @@
+export default [
+  {
+    city: { en: "Ivano-Frankivsk", uk: "Івано-Франківськ" },
+    street: { en: "Makuhy str., 6", uk: "вул. Макухи, 6" },
+    phoneOfficeVodafone: "+38 050 377 08 01",
+    phoneOfficeKyivstar: "+38 067 327 08 01",
+    phoneOffice: "+38 0342 522 127",
+  },
+  {
+    city: { en: "Kyiv", uk: "Київ" },
+    street: {
+      en: "Simji Sosninykh str., 9 (Electrotechnical factory)",
+      uk: "вул. Сім'ї Сосніних, 9 (Електротехнічний завод)",
+    },
+    phoneOfficeKyivstar: "+38 067 009 08 01",
+  },
+  {
+    city: { en: "Dnipro", uk: "Дніпро" },
+    street: {
+      en: "Akinfieva str., 18",
+      uk: "вул. Акінфієва, 18",
+    },
+    phoneOfficeVodafone: "+38 050 433 28 38",
+  },
+  {
+    city: { en: "Kharkiv", uk: "Харків" },
+    street: {
+      en: "Plekhanivska str., 117",
+      uk: "вул. Плеханівська, 117",
+    },
+    phoneOfficeVodafone: "+38 050 433 25 66",
+  },
+  {
+    name: { en: "Ukrinvestprylad", uk: "Укрінвестприлад" },
+    city: { en: "Lviv", uk: "Львів" },
+    street: { en: "Zelena str., 251", uk: "вул. Зелена, 251" },
+    phoneOfficeVodafone: "+38 050 317 03 87",
+    phoneOffice: "+38 0322 70 91 91",
+  },
+]
