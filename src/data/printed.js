d086c19434028a7baf79df432a8e5ca72ffcfd6f
Diana <diana.didijtsuk@gmail.c0m>
2020-08-18 21:27:17 +0300

    changed text in printed

--- src/data/printed.js
+++ src/data/printed.js
@@ -4,9 +4,9 @@ export default [
     title: { en: "Print on envelope", uk: "Друк по готових конвертах" },
     text: {
       en:
-        "This is a convenient and fast way to make envelopes with a logo or a small print, but printing on the entire area and on the edges is impossible - the machine needs a gap of 5 mm. A maximum of 4 colors can be selected.",
+        "This is a convenient way to print on envelopes, but printing on the entire area and on the edges is impossible - the machine needs a gap of 5-10 mm. Maximum colors - 4.",
       uk:
-        "Це зручний і швидкий спосіб виготовлення конвертів з логотипом або ж дрібним малюнком, однак друк на всій площі і по краях неможливий — машині потрібен відступ 5 мм. Максимум можливо вибрати 4 кольори.",
+        "Це зручний спосіб нанесення друку на конверти, однак друк на всій площі і по краях неможливий — машині потрібен відступ 5-10 мм. Максимальна кількість кольорів - 4.",
     },
     id: "printReadyEnv",
   },
@@ -15,16 +15,21 @@ export default [
     title: { en: "Print on carved", uk: "Друк на викроях" },
     text: {
       en:
-        "In this case, print is applied to sheets of paper, after which the press is cut it with a knife, which the machine makes into an envelope. This type of printing is almost unlimited - a choice of 6 colors on the outside + internal printing (4 colors).",
+        "First, print is applied to sheets of paper, after that press cut them, then machine makes it into an envelope. Maximun colors - 6 on the outside and inside.",
       uk:
-        "У цьому випадку друк наноситься на листи паперу, після чого на пресі ножем висікаються викрої, які машина згортає в конверт. Цей вид друку практично нічим необмежений — на вибір 6 кольорів ззовні + внутрішній друк (4 кольори).",
+        "Спочатку друк наноситься на листи паперу, потім пресом висікаються викрої, далі машина згортає їх в конверт. Максимальна кількість кольорів - 6 з зовнішньої та внутрішньої сторін.",
     },
     id: "printCutoutEnv",
   },
   {
     img: "/products/digital-print.png",
     title: { en: "Digital print", uk: "Цифровий друк" },
-    text: { en: "Digital full color print", uk: "Цифровий повноколірний друк" },
+    text: {
+      en:
+        "It is a fast way of full color printing on paper bags, banderole bags and other products, which can't be offset printed.",
+      uk:
+        "Це швидкий спосіб повноколірного друку на паперових торбах, бандерольних пакетах та інших виробах, які не можна задрукувати офсетним друком.",
+    },
     id: "printDigital",
   },
 ]
