f0bc9c5aa2d18c356b15914594de80efd2618b98
Taras Chuiko <taraschuiko@gmail.com>
2020-10-06 16:01:31 +0300

    Made products page items fitting the screen height

--- src/pages/products.js
+++ src/pages/products.js
@@ -48,13 +48,24 @@ export default props => {
           `}
         >
           {products.map(({ name, img, id }) => (
-            <div className="w-full md:w-1/2 px-2">
+            <div
+              className="w-full md:w-1/2 px-2"
+              css={css`
+                height: 240px;
+                @media screen and (max-width: 1024px) {
+                  height: calc((100vh - 223px) / ${products.length});
+                  min-height: 75px;
+                }
+              `}
+            >
               <LocalizedLink
                 key={id}
                 to={`/${id}`}
                 css={css`
-                  display: block;
-                  height: 240px;
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                  height: 100%;
                   background: url(${img}), rgba(128, 128, 128, 0.4);
                   position: relative;
                   text-align: center;
@@ -64,15 +75,14 @@ export default props => {
                   font-weight: bold;
                   font-size: 40px;
                   text-decoration: none;
+                  @media screen and (max-width: 1024px) {
+                    font-size: 22px;
+                  }
                 `}
               >
                 <h5
                   css={css`
                     font-family: "Raleway", sans-serif;
-                    position: absolute;
-                    top: 35%;
-                    left: 50%;
-                    transform: translate(-50%, -50%);
                   `}
                 >
                   {t(name)}
