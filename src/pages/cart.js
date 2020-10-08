6d58bc0ec8c2328fb0d190fe99e9e015ef0dcd29
Diana <diana.didijtsuk@gmail.c0m>
2020-10-08 15:58:08 +0300

    basic request as formik

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -3,22 +3,105 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import { Form, Formik, useField } from "formik"
+import * as Yup from "yup"
+import { localizedNavigate } from "../components/LocalizedLink"
+
 import { css } from "@emotion/core"
 
 import { useTranslation } from "react-i18next"
 
 import BtnNext from "../components/BtnNext"
 import ContactForm from "../components/ContactForm"
+import Steps from "../components/Steps"
 
 import logoGrey from "../images/logoGrey.png"
 
+const MyInput = ({ label, ...props }) => {
+  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+  // which we can spread on <input> and alse replace ErrorMessage entirely.
+  const [field, meta] = useField(props)
+  return (
+    <div
+      css={css`
+        display: flex;
+        flex-direction: column;
+        margin-top: 2rem;
+      `}
+    >
+      <label
+        htmlFor={props.id || props.name}
+        css={css`
+          font-size: 12px;
+          padding-bottom: 5px;
+        `}
+      >
+        {label}
+      </label>
+      <input
+        {...field}
+        {...props}
+        css={css`
+          height: 65px;
+          background: #ffffff;
+          border: 1px solid #d6d6d6;
+          border-radius: 3px;
+          box-sizing: border-box;
+          padding: 20px;
+          outline: none;
+          ::-webkit-input-placeholder {
+            font-size: 16px;
+            color: #ababab;
+          }
+          :focus {
+            border: 2px solid #c4c4c4;
+            border-radius: 3px;
+          }
+        `}
+      />
+      {meta.touched && meta.error ? (
+        <div
+          css={css`
+            width: 141px;
+            height: 35px;
+            background: #a74444;
+            border-radius: 3px;
+            color: white;
+            color: #ffffff;
+            font-size: 12px;
+            line-height: 14px;
+            text-align: center;
+          `}
+        >
+          {meta.error}
+        </div>
+      ) : null}
+    </div>
+  )
+}
+
+const sendEmail = async values => {
+  return fetch("https://hooks.zapier.com/hooks/catch/8670957/og9qtfo/", {
+    method: "POST",
+    body: JSON.stringify(values),
+  })
+}
+
 export default props => {
+  const [step, setStep] = React.useState(1)
+
   const T = useTranslation()
   if (T.i18n.language !== props.pageContext.langKey) {
     T.i18n.changeLanguage(props.pageContext.langKey)
   }
 
   const t = key => (typeof key === "string" ? T.t(key) : key[T.i18n.language])
+
+  const steps = [
+    t("requestType"),
+    t("requestOrderCreate"),
+    t("requestPersonal"),
+  ]
 
   return (
     <Layout>
@@ -35,117 +118,178 @@ export default props => {
         `}
         to="/"
       ></Link>
-      <section
-        css={css`
-          display: flex;
-          flex-directon: row;
-          justify-content: center;
-          font-size: 14px;
-          font-weight: bold;
-          margin: 80px auto 50px auto;
-          @media (max-width: 1024px) {
-            margin-top: 120px;
-            padding: 0 10px;
-          }
-        `}
+
+      <Steps steps={steps} activeStep={step} />
+
+      <Formik
+        initialValues={{
+          product: "",
+          amount: "",
+          message: "",
+          name: "",
+          company: "",
+          city: "",
+          email: "",
+          phone: "",
+        }}
+        validationSchema={Yup.object({
+          name: Yup.string().required("Required"),
+          email: Yup.string().required("Required"),
+          phone: Yup.string().required("Required"),
+        })}
+        onSubmit={async () => {
+          await sendEmail()
+          localizedNavigate("/thanxrequest", props.pageContext.langKey)
+        }}
       >
-        <div
-          css={css`
-            color: #383838;
-            border-bottom: 3px solid #383838;
-            padding: 10px 20px;
-            display: flex;
-            align-items: center;
-          `}
-        >
-          <span
+        {({ isSubmitting, setFieldValue }) => (
+          <Form
             css={css`
-              font-size: 26px;
-              padding-right: 5px;
+              width: 90vw;
+              max-width: 780px;
+              margin: 0 auto;
             `}
           >
-            1
-          </span>
-          {t("requestType")}
-        </div>
-        <div
-          css={css`
-            color: #b40039;
-            border-bottom: 3px solid #b40039;
-            padding: 10px 20px;
-            display: flex;
-            align-items: center;
-          `}
-        >
-          <span
-            css={css`
-              font-size: 26px;
-              padding-right: 5px;
-            `}
-          >
-            2
-          </span>
-          {t("requestOrderCreate")}
-        </div>
-        <div
-          css={css`
-            color: #b4b4b4;
-            border-bottom: 3px solid #b4b4b4;
-            padding: 10px 20px;
-            display: flex;
-            align-items: center;
-          `}
-        >
-          <span
-            css={css`
-              font-size: 26px;
-              padding-right: 5px;
-            `}
-          >
-            3
-          </span>
-          {t("requestPersonal")}
-        </div>
-      </section>
-      <h1
-        css={css`
-          font-size: 40px;
-          font-weight: bold;
-          margin-bottom: 50px;
-          text-align: center;
-        `}
-      >
-        {t("basicOrder")}
-      </h1>
-      <section
-        css={css`
-          width: 50%;
-          height: 350px;
-          background: #d6d6d6;
-          border-radius: 3px;
-          position: absolute;
-          left: 50%;
-          -webkit-transform: translate(-50%, -2%);
-          transform: translate(-50%, -2%);
-        `}
-      >
-        <div
-          css={css`
-            width: 100%;
-            background: #8a8a8a;
-            border-radius: 0px 0px 3px 3px;
-            height: 78px;
-            position: absolute;
-            bottom: 0;
-            text-align: right;
-            padding-right: 37px;
-            padding-top: 28px;
-          `}
-        >
-          <BtnNext />
-        </div>
-      </section>
-      <ContactForm />
+            <h1
+              css={css`
+                font-size: 40px;
+                font-weight: bold;
+                margin-bottom: 50px;
+                text-align: center;
+              `}
+            >
+              {t("basicOrder")}
+            </h1>
+            {step === 1 && (
+              <>
+                <section
+                  css={css`
+                    width: 50%;
+                    height: 350px;
+                    background: #d6d6d6;
+                    border-radius: 3px;
+                    position: absolute;
+                    left: 50%;
+                    -webkit-transform: translate(-50%, -2%);
+                    transform: translate(-50%, -2%);
+                  `}
+                >
+                  <div
+                    css={css`
+                      width: 100%;
+                      background: #8a8a8a;
+                      border-radius: 0px 0px 3px 3px;
+                      height: 78px;
+                      position: absolute;
+                      bottom: 0;
+                      text-align: right;
+                      padding-right: 37px;
+                      padding-top: 28px;
+                    `}
+                  >
+                    <BtnNext type="button" onClick={() => setStep(2)} />
+                  </div>
+                </section>
+              </>
+            )}
+            {step === 2 && (
+              <>
+                <div
+                  css={css`
+                    width: 90vw;
+                    max-width: 780px;
+                    margin: 0 auto;
+                    display: flex;
+                    justify-content: space-between;
+                    @media screen and (max-width: 768px) {
+                      flex-direction: column;
+                      padding-bottom: 80px;
+                    }
+                  `}
+                >
+                  <div
+                    css={css`
+                      width: 48%;
+                      @media screen and (max-width: 768px) {
+                        width: 100%;
+                      }
+                    `}
+                  >
+                    <MyInput
+                      label={t("name")}
+                      name="name"
+                      type="text"
+                      placeholder={t("name")}
+                    />
+                    <MyInput
+                      label={t("company")}
+                      name="company"
+                      type="text"
+                      placeholder={t("company")}
+                    />
+                  </div>
+                  <div
+                    css={css`
+                      width: 48%;
+                      @media screen and (max-width: 768px) {
+                        width: 100%;
+                      }
+                    `}
+                  >
+                    <MyInput
+                      label={t("city")}
+                      name="city"
+                      type="text"
+                      placeholder={t("city")}
+                    />
+                    <MyInput
+                      label="Email"
+                      name="email"
+                      type="email"
+                      placeholder="hello@email.com"
+                    />
+                    <MyInput
+                      label={t("phone")}
+                      name="phone"
+                      type="phone"
+                      placeholder="123456789"
+                    />
+                  </div>
+                </div>
+                <button
+                  css={css`
+                    background: #c5003e;
+                    border-radius: 3px;
+                    border: none;
+                    padding-top: 18px;
+                    padding-bottom: 18px;
+                    color: white;
+                    cursor: pointer;
+                    font-weight: 500;
+                    font-size: 18px;
+                    outline: none;
+                    text-decoration: none;
+                    display: block;
+                    text-align: center;
+                    margin: 30px 0;
+                    width: 100%;
+                    &:hover {
+                      background: #b60039;
+                    }
+                    &:active {
+                      background: #f4004d;
+                    }
+                  `}
+                  type="submit"
+                  // disabled={isSubmitting}
+                >
+                  {t("request")}
+                </button>
+              </>
+            )}
+          </Form>
+        )}
+      </Formik>
     </Layout>
   )
 }
