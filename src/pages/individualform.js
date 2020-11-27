defb4373b8451b1d72db2d9430f1733c6fb2e80c
Diana <diana.didijtsuk@gmail.c0m>
2020-11-27 17:06:20 +0200

    print form and individual form step 2 remade

--- src/pages/individualform.js
+++ src/pages/individualform.js
@@ -19,69 +19,6 @@ import logoGrey from "../images/logoGrey.png"
 import ContactForm from "../components/ContactForm"
 import BtnSendForm from "../components/BtnSendForm"
 
-const MyInput = ({ label, ...props }) => {
-  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-  // which we can spread on <input> and alse replace ErrorMessage entirely.
-  const [field, meta] = useField(props)
-  return (
-    <div
-      css={css`
-        display: flex;
-        flex-direction: column;
-        margin-top: 2rem;
-      `}
-    >
-      <label
-        htmlFor={props.id || props.name}
-        css={css`
-          font-size: 12px;
-          padding-bottom: 5px;
-        `}
-      >
-        {label}
-      </label>
-      <input
-        {...field}
-        {...props}
-        css={css`
-          height: 65px;
-          background: #ffffff;
-          border: 1px solid #d6d6d6;
-          border-radius: 3px;
-          box-sizing: border-box;
-          padding: 20px;
-          outline: none;
-          ::-webkit-input-placeholder {
-            font-size: 16px;
-            color: #ababab;
-          }
-          :focus {
-            border: 2px solid #c4c4c4;
-            border-radius: 3px;
-          }
-        `}
-      />
-      {meta.touched && meta.error ? (
-        <div
-          css={css`
-            width: 141px;
-            height: 35px;
-            background: #a74444;
-            border-radius: 3px;
-            color: white;
-            color: #ffffff;
-            font-size: 12px;
-            line-height: 14px;
-            text-align: center;
-          `}
-        >
-          {meta.error}
-        </div>
-      ) : null}
-    </div>
-  )
-}
-
 const MyMessageInput = ({ ...props }) => {
   const [field, meta] = useField(props)
   return (
@@ -260,97 +197,12 @@ export default props => {
             )}
             {step === 2 && (
               <>
-                <div
-                  css={css`
-                    width: 90vw;
-                    max-width: 780px;
-                    margin: 0 auto;
-                    display: flex;
-                    justify-content: space-between;
-                    @media screen and (max-width: 768px) {
-                      flex-direction: column;
-                      padding-bottom: 80px;
-                    }
-                  `}
-                >
-                  <div
-                    css={css`
-                      width: 48%;
-                      @media screen and (max-width: 768px) {
-                        width: 100%;
-                      }
-                    `}
-                  >
-                    <MyInput
-                      label={t("name")}
-                      name="name"
-                      type="text"
-                      placeholder={t("name")}
-                    />
-                    <MyInput
-                      label={t("company")}
-                      name="company"
-                      type="text"
-                      placeholder={t("company")}
-                    />
-                  </div>
-                  <div
-                    css={css`
-                      width: 48%;
-                      @media screen and (max-width: 768px) {
-                        width: 100%;
-                      }
-                    `}
-                  >
-                    <MyInput
-                      label={t("city")}
-                      name="city"
-                      type="text"
-                      placeholder={t("city")}
-                    />
-                    <MyInput
-                      label="Email"
-                      name="email"
-                      type="email"
-                      placeholder="hello@email.com"
-                    />
-                    <MyInput
-                      label={t("phone")}
-                      name="phone"
-                      type="phone"
-                      placeholder="123456789"
-                    />
-                  </div>
-                </div>
-                <button
-                  css={css`
-                    background: #c5003e;
-                    border-radius: 3px;
-                    border: none;
-                    padding-top: 18px;
-                    padding-bottom: 18px;
-                    color: white;
-                    cursor: pointer;
-                    font-weight: 500;
-                    font-size: 18px;
-                    outline: none;
-                    text-decoration: none;
-                    display: block;
-                    text-align: center;
-                    margin: 30px 0;
-                    width: 100%;
-                    &:hover {
-                      background: #b60039;
-                    }
-                    &:active {
-                      background: #f4004d;
-                    }
-                  `}
-                  type="submit"
+                <ContactForm />
+
+                <BtnSendForm
                   disabled={isSubmitting}
-                >
-                  {t("request")}
-                </button>
+                  className={isSubmitting && "progress"}
+                />
               </>
             )}
           </Form>
--- src/pages/printform.js
+++ src/pages/printform.js
@@ -16,72 +16,7 @@ import arrowSelect from "../images/arrow-select.png"
 import BtnSendForm from "../components/BtnSendForm"
 import BtnNext from "../components/BtnNext"
 import Steps from "../components/Steps"
-
-// import i18n from "i18next"
-// i18n.changeLanguage("en")
-
-const MyInput = ({ label, ...props }) => {
-  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-  // which we can spread on <input> and alse replace ErrorMessage entirely.
-  const [field, meta] = useField(props)
-  return (
-    <div
-      css={css`
-        display: flex;
-        flex-direction: column;
-        margin-top: 2rem;
-      `}
-    >
-      <label
-        htmlFor={props.id || props.name}
-        css={css`
-          font-size: 12px;
-          padding-bottom: 5px;
-        `}
-      >
-        {label}
-      </label>
-      <input
-        {...field}
-        {...props}
-        css={css`
-          height: 65px;
-          background: #ffffff;
-          border: 1px solid #d6d6d6;
-          border-radius: 3px;
-          box-sizing: border-box;
-          padding: 20px;
-          outline: none;
-          ::-webkit-input-placeholder {
-            font-size: 16px;
-            color: #ababab;
-          }
-          :focus {
-            border: 2px solid #c4c4c4;
-            border-radius: 3px;
-          }
-        `}
-      />
-      {meta.touched && meta.error ? (
-        <div
-          css={css`
-            width: 141px;
-            height: 35px;
-            background: #a74444;
-            border-radius: 3px;
-            color: white;
-            color: #ffffff;
-            font-size: 12px;
-            line-height: 14px;
-            text-align: center;
-          `}
-        >
-          {meta.error}
-        </div>
-      ) : null}
-    </div>
-  )
-}
+import ContactForm from "../components/ContactForm"
 
 const MySelect = ({ label, ...props }) => {
   const [field, meta] = useField(props)
@@ -313,13 +248,6 @@ const MyAmountInput = ({ label, setFieldValue, ...props }) => {
   )
 }
 
-const sendEmail = async values => {
-  return fetch("https://hooks.zapier.com/hooks/catch/8670957/og9qtfo/", {
-    method: "POST",
-    body: JSON.stringify(values),
-  })
-}
-
 export default props => {
   const [step, setStep] = React.useState(1)
 
@@ -382,7 +310,7 @@ export default props => {
           phone: Yup.string().required("Required"),
         })}
         onSubmit={async () => {
-          await sendEmail()
+          // await sendEmail()
           localizedNavigate("/thanxrequest", props.pageContext.langKey)
         }}
       >
@@ -479,97 +407,12 @@ export default props => {
 
             {step === 2 && (
               <>
-                <div
-                  css={css`
-                    width: 90vw;
-                    max-width: 780px;
-                    margin: 0 auto;
-                    display: flex;
-                    justify-content: space-between;
-                    @media screen and (max-width: 768px) {
-                      flex-direction: column;
-                      padding-bottom: 80px;
-                    }
-                  `}
-                >
-                  <div
-                    css={css`
-                      width: 48%;
-                      @media screen and (max-width: 768px) {
-                        width: 100%;
-                      }
-                    `}
-                  >
-                    <MyInput
-                      label={t("name")}
-                      name="name"
-                      type="text"
-                      placeholder={t("name")}
-                    />
-                    <MyInput
-                      label={t("company")}
-                      name="company"
-                      type="text"
-                      placeholder={t("company")}
-                    />
-                  </div>
-                  <div
-                    css={css`
-                      width: 48%;
-                      @media screen and (max-width: 768px) {
-                        width: 100%;
-                      }
-                    `}
-                  >
-                    <MyInput
-                      label={t("city")}
-                      name="city"
-                      type="text"
-                      placeholder={t("city")}
-                    />
-                    <MyInput
-                      label="Email"
-                      name="email"
-                      type="email"
-                      placeholder="hello@email.com"
-                    />
-                    <MyInput
-                      label={t("phone")}
-                      name="phone"
-                      type="phone"
-                      placeholder="123456789"
-                    />
-                  </div>
-                </div>
-                <button
-                  css={css`
-                    background: #c5003e;
-                    border-radius: 3px;
-                    border: none;
-                    padding-top: 18px;
-                    padding-bottom: 18px;
-                    color: white;
-                    cursor: pointer;
-                    font-weight: 500;
-                    font-size: 18px;
-                    outline: none;
-                    text-decoration: none;
-                    display: block;
-                    text-align: center;
-                    margin: 30px 0;
-                    width: 100%;
-                    &:hover {
-                      background: #b60039;
-                    }
-                    &:active {
-                      background: #f4004d;
-                    }
-                  `}
-                  type="submit"
+                <ContactForm />
+
+                <BtnSendForm
                   disabled={isSubmitting}
-                >
-                  {t("request")}
-                </button>
+                  className={isSubmitting && "progress"}
+                />
               </>
             )}
           </Form>
